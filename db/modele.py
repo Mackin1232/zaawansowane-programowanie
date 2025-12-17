4d22deb64f96c9102b31dd19eb038f877212d31d
Maciej Kindrat <91846198+Mackin1232@users.noreply.github.com>
2025-12-17 09:05:03 +0100

    Dodanie tabel Airports i Bookings

--- db/modele.py
+++ db/modele.py
@@ -24,6 +24,21 @@ class User(db.Model):
     def to_dict(self):
         return {"id": self.id, "username": self.username, "password": self.password, "email": self.email, "role": self.role}
     
+# -------------------- AIRPORT - lotniska (w Polsce i miejscach, które są celem podróży) --------------------
+
+class newAirport(BaseModel):
+    name: str
+    iata: str
+    #icao: str
+
+class Airport(db.Model):
+    __tablename__="airports"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    iata = db.Column(db.String, unique=True, nullable=False)
+
+    def to_dict(self):
+        return {"id": self.id, "name": self.name, "iata": self.iata}
 
 # -------------------- LOCATION - hotele, domki itd. ------------------
 
@@ -43,3 +58,32 @@ class Location(db.Model):
 
     def to_dict(self):
         return {"id": self.id, "country": self.country, "city": self.city, "airport": self.airport, "desc": self.desc}
+
+
+# ---------------------- BOOKING - rezerwacje --------------------
+
+class newBooking(BaseModel):
+    bookingId: str
+    userId: str
+    flightId: str
+    locationId: str
+    departureDate: str
+    returnDate: str
+
+class Booking(db.Model): # TODO: dodać relacje z innymi tabelami
+    __tablename__="bookings"
+    bookingId = db.Column(db.String, primary_key=True, autoincrement=True)
+    userId = db.Column(db.String, nullable=False)
+    flightId = db.Column(db.String, nullable=False)
+    locationId = db.Column(db.String, nullable=False)
+    departureDate = db.Column(db.String, nullable=False)
+    returnDate = db.Column(db.String, nullable=False)
+
+    def to_dict(self):
+        return {"bookingId": self.bookingId, "userId": self.userId, "flightId": self.flightId, "locationId": self.locationId, "departureDate": self.departureDate, "returnDate": self.returnDate}
+
+
+# ------------------------- FLIGHT - loty (z API, można zasymulować dane [ np. dane z API powielić wiele razy dla różnych dat ] ) ---------------
+
+# TODO: zrobic tabele na podstawie API (AirLabs?)
+    
